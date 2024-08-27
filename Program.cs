f1c8606f6fd6a1d87acf3d08b4a6289b8a2f8c93
romansechin <romansechin@gmail.com>
2024-08-27 15:11:11 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Text;
+using System.Text.Json;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        File.WriteAllText("eureka.txt", "םולש!", Encoding.Unicode);
+        File.WriteAllText("elephant1.txt", "\uD83D\uDC18");
+        File.WriteAllText("elephant2.txt", "\U0001F418");
+        byte[] eurekaBytes = File.ReadAllBytes("eureka.txt");
+        foreach (byte b in eurekaBytes) {
+            Console.Write("{0:x2} ", b);
+        }
+        Console.WriteLine();
+        Console.WriteLine(Encoding.UTF8.GetString(eurekaBytes));
+        Console.WriteLine(JsonSerializer.Serialize("ש"));
+    }
+}
